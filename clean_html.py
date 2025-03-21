1c519dea7cf194a97ae5874026f11ca2bf1936cf
ogjex <m.agnu.ha@gmail.com>
2025-03-21 13:16:59 +0100

    add start of beautifulsoup to extract lessons

--- clean_html.py
+++ clean_html.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 import os
 import sys
 
@@ -7,8 +8,14 @@ def clean_html(file:str):
     if not os.path.exists(file_path):
         print(f"file not found: {file_path}")
         os._exit(0)
-    else:
-        print(f"path to file found: {file_path}")
+    print(f"path to file found: {file_path}")
+    try:
+    with open(file_path, "r") as file:
+       course_content = file.read()
+       soup = BeautifulSoup(course_content, "html_parser")
+    lessons_divs = soup.find_all('div', class_='lessons')
+# ENDING HERE for today!
+
 # make the main method that counts arguments
 if __name__ == "__main__":
     if len(sys.argv) != 2:
